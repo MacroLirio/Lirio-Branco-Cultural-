c3f8b4b6e58626db0acc49ab6e150e00530c7810
MacroLirio <liriobrancocultural@gmail.com>
2025-08-28 11:49:16 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,15 @@
-// Cole aqui seu config do Firebase (Console > Configurações do projeto > Apps > Web).
-// Exemplo de estrutura (substitua pelos seus valores):
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SUA_MEASUREMENT_ID"
+  apiKey: "cole aqui",
+  authDomain: "cole aqui",
+  projectId: "cole aqui",
+  storageBucket: "cole aqui",
+  messagingSenderId: "cole aqui",
+  appId: "cole aqui",
+  measurementId: "cole aqui"
 };
 
-// Inicialização
+// Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
