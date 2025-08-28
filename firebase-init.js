0902bd127d7b1fbded4c646c4788994cfe406e0f
MacroLirio <liriobrancocultural@gmail.com>
2025-08-28 13:56:21 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,15 @@
 // Cole aqui seu config do Firebase (Console > Configurações do projeto > Apps > Web).
 // Exemplo de estrutura (substitua pelos seus valores):
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SUA_MEASUREMENT_ID"
+  apiKey: "AIzSyXXXXXXX",
+  authDomain: "sistema-lirio-branco.firebaseapp.com",
+  projectId: "sistema-lirio-branco",
+  storageBucket: "sistema-lirio-branco.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456",
+  measurementId: "G-XXXXXXX"
 };
+
 
 // Inicialização
 firebase.initializeApp(firebaseConfig);
